8b4007ed0f23ad69c6380785bb5924da075f3a69
Irina Novakovich <irinaredhood@gmail.com>
2022-02-18 19:04:22 +0300

    added main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Navbar/Profile/Profile";
+import Dialogs from "./components/Navbar/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import News from "./components/Navbar/News/News";
+import Music from "./components/Navbar/Music/Music";
+import Settings from "./components/Navbar/Settings/Settings";
+import {StateProps} from "./state/State";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = (props: StateProps) => {
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar sidebar={props.state.sidebar}  />
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path={'/profile'} element={<Profile posts={props.state.posts} addPost={props.addPost} newPostText={props.state.newPostText}/>}/>
+                        <Route path={'/dialogs/*'} element={<Dialogs dialogs={props.state.dialogs}
+                                                                     messages={props.state.messages}/>}/>
+                        <Route path={'/news'} element={<News/>}/>
+                        <Route path={'/music'} element={<Music/>}/>
+                        <Route path={'/settings'} element={<Settings/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
