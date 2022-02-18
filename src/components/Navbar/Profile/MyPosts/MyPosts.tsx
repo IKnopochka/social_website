9bc5a5bddb57aeef21830e72c24579a94df1f608
Irina Novakovich <irinaredhood@gmail.com>
2022-02-18 19:02:11 +0300

    added my posts on profile page component

---
+++ src/components/Navbar/Profile/MyPosts/MyPosts.tsx
@@ -0,0 +1,40 @@
+import classes from "./MyPosts.module.css";
+import React from "react";
+import Post from "./Post/Post";
+import {PostsPropsType} from "../../../../state/State";
+
+const MyPosts = (props: PostsPropsType) => {
+
+    let postElements = props.posts.map(p => (<Post id={p.id} message={p.message} likeCount={p.likeCount}/>))
+
+    let newPostElement = React.createRef<HTMLTextAreaElement>();
+
+    const onAddPost = () => {
+        if(newPostElement.current) {
+            props.addPost(newPostElement.current.value);
+            newPostElement.current.value = '';
+        }
+    }
+
+    const onPostChange = () => {}
+
+    return (
+        <div className={classes.textArea}>
+            my posts
+            <div>
+                <div>
+                    <textarea ref={newPostElement} onChange={onPostChange} value={props.newPostText}/>
+                </div>
+                <div>
+                    <button onClick={onAddPost}>Add post</button>
+                    <button>Remove</button>
+                </div>
+            </div>
+            <div className={classes.posts}>
+                {postElements}
+            </div>
+        </div>
+    )
+}
+
+export default MyPosts;
