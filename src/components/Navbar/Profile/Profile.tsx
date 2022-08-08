89d66ea195ed75919f73782042bab0cf5cef8f89
Irina Novakovich <irinaredhood@gmail.com>
2022-08-08 13:46:13 +0300

    45 lesson samuray way Затипизировали

--- src/components/Navbar/Profile/Profile.tsx
+++ src/components/Navbar/Profile/Profile.tsx
@@ -1,19 +1,16 @@
 import React from "react";
-import MyPosts from "./MyPosts/MyPosts";
 import classes from './Profile.module.css';
 import ProfileInfo from "./ProfileInfo/ProfileInfo"
-import { ReduxStorePropsType} from "../../../state/store";
-import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import SuperMyPostsContainer from "./MyPosts/MyPostsContainer";
 
 
 
 
 const Profile = () => {
-    debugger
     return (
         <div className={classes.content}>
             <ProfileInfo/>
-            <MyPostsContainer/>
+            <SuperMyPostsContainer/>
             {/*<MyPosts posts={props.posts} newPostText={props.newPostText} dispatch={props.dispatch}/>*/}
         </div>
     )
